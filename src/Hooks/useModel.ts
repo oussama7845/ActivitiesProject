056fbe53d1121f52f9f4403891f877c029485c13
oussama Abakarim <abakarimoussama@gmail.com>
2023-02-08 17:00:03 +0000

    first commit

---
+++ src/Hooks/useModel.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "react-query";
+
+async function fetchModel(codigo:number) {
+  const result = await fetch(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${codigo}/modelos`);
+  const json = await result.json();
+  return json;
+}
+
+export function useModel(codigo: number) {
+  return useQuery(["Model"], () =>fetchModel(codigo));
+}
